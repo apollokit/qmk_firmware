80cc1dcd7ce56279839b38eafdc3df63cc3fa88d
Kit Kennedy <kit@vincilab.co>
2019-10-16 10:57:45 -0700

    Updates to tapping term for modifier keyt

--- keyboards/ergodox_ez/keymaps/apollokit/config.h
+++ keyboards/ergodox_ez/keymaps/apollokit/config.h
@@ -2,6 +2,9 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#undef TAPPING_TERM
+#define TAPPING_TERM 300
+
 #define NO_AUTO_SHIFT_ALPHA
 #define DISABLE_RGB_MATRIX_DIGITAL_RAIN
 #define DISABLE_RGB_MATRIX_ALPHAS_MODS
